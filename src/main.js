d235c48161c819dff3b3af0b1f2b9c04a5cd4708
itcast <itheima@itcast.cn>
2021-09-10 17:24:30 +0800

    项目初始化 引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载vant组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import '@/style/index.less'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
